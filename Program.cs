ef75d0d14d559c7e2279baf19cc860d1daec4dc7
Pilumins <109856579+Pilumins@users.noreply.github.com>
2025-03-13 12:45:02 +0000

    Revert "Development"

--- Program.cs
+++ Program.cs
@@ -10,22 +10,11 @@ namespace DungeonExplorer
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Game game = new Game();
-                game.Start();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("An error occurred: " + ex.Message);
-            }
-            finally
-            {
-                Console.WriteLine("thanks for playing Dungeon Explorer!");
-                Console.WriteLine("Press any key to exit...");
-                Console.ReadKey();
-            }
+            Game game = new Game();
+            game.Start();
+            Console.WriteLine("Waiting for your Implementation");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
-
